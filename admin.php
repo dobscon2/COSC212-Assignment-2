58ff4e0e4de0d9096eaa9ea316aab7d10b225b3f
Connor Dobson <dobco402@student.otago.ac.nz>
2020-09-30 21:37:22 +1300

    Working on decoding JSON data in PHP

--- admin.php
+++ admin.php
@@ -4,9 +4,16 @@
 ?>
         <section>
             <h2>Confirmed Bookings</h2>
-            <table id="bookings"></table>
-        </section>
+            <table id="bookings">
+                <?php
+                    $json_file = file_get_contents('Resources/bookings.json');
+                    $bookings = json_decode($json_file, true);
+                    $bookings = $bookings['bookings']['booking'];
 
+                    print(sizeof($bookings));
+                ?>
+            </table>
+        </section>
 <?php
     include('Resources/Private/footer.php');
 ?>
